7e7f6d6c52dd63cfae050b77961b07705cc62a7b
paraloq <>
2023-02-23 19:36:49 +0000

    Update from https://github.com/paraloq/vendor_widget/commit/35fad95e2277537c5d8be5426cc1491eefe79b1e

--- module/src/module.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const ChatWindow: import("vue").VueElementConstructor<{}>;
-export declare const installWidget: () => void;
-export declare function register(tagName?: string): Promise<void>;
