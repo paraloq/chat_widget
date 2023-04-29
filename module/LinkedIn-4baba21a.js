33b0678f1794d466afb0bb4f73437e2a768f16db
paraloq <>
2023-04-29 18:10:21 +0000

    Update from https://github.com/paraloq/vendor_widget/commit/fae929eea17f18f9bbe422ea68c9ce71d09c4493

---
+++ module/LinkedIn-4baba21a.js
@@ -0,0 +1,32 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, a as createBaseVNode } from "./module-c86eb67e.js";
+const _sfc_main = {};
+const _hoisted_1 = {
+  class: "text-[#0077b5]",
+  viewBox: "0 0 39 39",
+  fill: "none",
+  xmlns: "http://www.w3.org/2000/svg"
+};
+const _hoisted_2 = /* @__PURE__ */ createBaseVNode("circle", {
+  cx: "19.5",
+  cy: "19.5",
+  r: "19.5",
+  fill: "currentColor"
+}, null, -1);
+const _hoisted_3 = /* @__PURE__ */ createBaseVNode("path", {
+  id: "LinkedIn_color",
+  d: "M18,20.1v6.655H14.142V20.549c0-1.56-.558-2.624-1.954-2.624a2.111,2.111,0,0,0-1.98,1.411,2.64,2.64,0,0,0-.128.941v6.481H6.221s.052-10.516,0-11.606H10.08V16.8c-.008.012-.018.026-.025.037h.025V16.8a3.832,3.832,0,0,1,3.478-1.918C16.1,14.88,18,16.539,18,20.1ZM2.184,9.558a2.011,2.011,0,1,0-.051,4.011h.026a2.012,2.012,0,1,0,.025-4.011ZM.229,26.758H4.087V15.152H.229Z",
+  transform: "translate(11 1.442)",
+  fill: "#fff"
+}, null, -1);
+const _hoisted_4 = [
+  _hoisted_2,
+  _hoisted_3
+];
+function _sfc_render(_ctx, _cache) {
+  return openBlock(), createElementBlock("svg", _hoisted_1, _hoisted_4);
+}
+const LinkedIn = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  LinkedIn as default
+};
+//# sourceMappingURL=LinkedIn-4baba21a.js.map
