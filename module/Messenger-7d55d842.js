583d96b7d5fe42f93092d858039723b2592485cd
paraloq <>
2023-04-29 18:10:34 +0000

    Update from https://github.com/paraloq/vendor_widget/commit/d0f0f2b5da104168a20e9377090bd0746b498f2f

---
+++ module/Messenger-7d55d842.js
@@ -0,0 +1,33 @@
+import { _ as _export_sfc, o as openBlock, c as createElementBlock, a as createBaseVNode } from "./module-5843ace3.js";
+const _sfc_main = {};
+const _hoisted_1 = {
+  viewBox: "0 0 39 39",
+  class: "text-[#1E88E5]",
+  xmlns: "http://www.w3.org/2000/svg"
+};
+const _hoisted_2 = /* @__PURE__ */ createBaseVNode("circle", {
+  class: "color-element",
+  cx: "19.4395",
+  cy: "19.4395",
+  r: "19.4395",
+  fill: "currentColor"
+}, null, -1);
+const _hoisted_3 = /* @__PURE__ */ createBaseVNode("path", {
+  "fill-rule": "evenodd",
+  "clip-rule": "evenodd",
+  d: "M0 9.63934C0 4.29861 4.68939 0 10.4209 0C16.1524 0 20.8418 4.29861 20.8418 9.63934C20.8418 14.98 16.1524 19.2787 10.4209 19.2787C9.37878 19.2787 8.33673 19.1484 7.42487 18.8879L3.90784 20.8418V17.1945C1.56311 15.3708 0 12.6353 0 9.63934ZM8.85779 10.1604L11.463 13.0261L17.1945 6.90384L12.1143 9.76959L9.37885 6.90384L3.64734 13.0261L8.85779 10.1604Z",
+  transform: "translate(9.01854 10.3146)",
+  fill: "white"
+}, null, -1);
+const _hoisted_4 = [
+  _hoisted_2,
+  _hoisted_3
+];
+function _sfc_render(_ctx, _cache) {
+  return openBlock(), createElementBlock("svg", _hoisted_1, _hoisted_4);
+}
+const Messenger = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  Messenger as default
+};
+//# sourceMappingURL=Messenger-7d55d842.js.map
